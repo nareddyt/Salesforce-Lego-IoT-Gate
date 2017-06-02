1fee132e8db0776a9fad5b17624bb16c96f0a79e
zhaoyuSFDC <zhaoyu.wang@salesforce.com>
2017-06-02 15:02:27 -0700

    Added post stuff, BROKEN

--- Lego_IoT_Gate.ino
+++ Lego_IoT_Gate.ino
@@ -1,12 +1,12 @@
 #include "Timer.h"
 #include "ESP8266WiFi.h"
 
-
 Timer t;
-
 // WiFi parameters to be configured
-const char* ssid = "YOUR WIFI NETWORK NAME";
-const char* password = "YOUR WIFI PASSWARD";
+const char* ssid = "exitroute";
+const char* password = "489YLQZV";
+const char* host = "http://iot-gate-proxy.azurewebsites.net/";
+WiFiClient client;
 
 // Constants
 const int leftLEDPin = D2;
@@ -18,7 +18,6 @@ const int rightLightPin = D5;
 
 // Enums
 enum state {IN, OUT};
-
 // Global vars
 int timeStamp = 0;
 int outCount = 1;
@@ -33,120 +32,113 @@ int leftTrig = false;
 int middleTrig = false;
 int rightTrig = false;
 
-//Post setup
-char serverName[] = "http://iot-gate-proxy.azurewebsites.net/";
-
-
 void setup() {
-  // set the digital pin as output:
-  pinMode(leftLEDPin, OUTPUT);
-  pinMode(middleLEDPin, OUTPUT);
-  pinMode(rightLEDPin, OUTPUT);
-  pinMode(leftLightPin, INPUT_PULLUP);
-  pinMode(middleLightPin, INPUT_PULLUP);
-  pinMode(rightLightPin, INPUT_PULLUP);
-
-  digitalWrite(leftLEDPin, HIGH);
-  digitalWrite(middleLEDPin, LOW);
-  digitalWrite(rightLEDPin, LOW);
-
-  // Start serial mode
-  Serial.begin(9600);
-
-  t.every(5000, changeState);
+    // set the digital pin as output:
+    pinMode(leftLEDPin, OUTPUT);
+    pinMode(middleLEDPin, OUTPUT);
+    pinMode(rightLEDPin, OUTPUT);
+    pinMode(leftLightPin, INPUT_PULLUP);
+    pinMode(middleLightPin, INPUT_PULLUP);
+    pinMode(rightLightPin, INPUT_PULLUP);
+    digitalWrite(leftLEDPin, HIGH);
+    digitalWrite(middleLEDPin, LOW);
+    digitalWrite(rightLEDPin, LOW);
+    // Start serial mode
+    Serial.begin(9600);
+    wifiSetup();
+    t.every(5000, changeState);
 }
 
 void loop() {
-  timeStamp = millis();
-  t.update();
-  
-  // Read input from light sensors
-  int leftLightInput = digitalRead(leftLightPin);
-  leftLightInput ^= 1;
-  int middleLightInput = digitalRead(middleLightPin);
-  int rightLightInput = digitalRead(rightLightPin);
-
-  // Check for spikes in values
-  if (leftHistory == 0 && !leftTrig) {
-    inCount += 1;
-    leftTrig = true;
-    Serial.println("Left gate triggered");
-  }
-  if (rightHistory == 0 && !rightTrig) {
-    outCount += 1;
-    rightTrig = true;
-    Serial.println("Right gate triggered");
-  }
-  if (middleHistory == 0 && !middleTrig) {
-    Serial.println("Middle gate triggered");
-    if (middleGateState == IN) {
+    timeStamp = millis();
+    t.update();
+    
+    // Read input from light sensors
+    int leftLightInput = digitalRead(leftLightPin);
+    leftLightInput ^= 1;
+    int middleLightInput = digitalRead(middleLightPin);
+    int rightLightInput = digitalRead(rightLightPin);
+    // Check for spikes in values
+    if (leftHistory == 0 && !leftTrig) {
       inCount += 1;
-    } else {
-      outCount += 1;
+      leftTrig = true;
+      Serial.println("Left gate triggered");
+      
     }
-    middleTrig = true;
-  }
-
-  // Reset triggers
-  if (leftHistory == 0b11111111111111111111 && leftTrig) {
-    leftTrig = false;
-    Serial.println("Left gate reset");
-  }
-  if (rightHistory == 0b11111111111111111111 && rightTrig) {
-    rightTrig = false;
-    Serial.println("Right gate reset");
-  }
-  if (middleHistory == 0b11111111111111111111 && middleTrig) {
-    Serial.println("Middle gate reset");
-    middleTrig = false;
-  }
-
-  // Update histories
-  leftHistory = leftHistory << 1;
-  leftHistory &= 0b11111111111111111111;
-  leftHistory |= leftLightInput;
-  middleHistory = middleHistory << 1;
-  middleHistory &= 0b11111111111111111111;;
-  middleHistory |= middleLightInput;
-  rightHistory = rightHistory << 1;
-  rightHistory &= 0b11111111111111111111;;
-  rightHistory |= rightLightInput;
+    if (rightHistory == 0 && !rightTrig) {
+      outCount += 1;
+      rightTrig = true;
+      Serial.println("Right gate triggered");
+    }
+    if (middleHistory == 0 && !middleTrig) {
+      Serial.println("Middle gate triggered");
+      if (middleGateState == IN) {
+        inCount += 1;
+      } else {
+        outCount += 1;
+      }
+      middleTrig = true;
+    }
+    // Reset triggers
+    if (leftHistory == 0b11111111111111111111 && leftTrig) {
+      leftTrig = false;
+      Serial.println("Left gate reset");
+    }
+    if (rightHistory == 0b11111111111111111111 && rightTrig) {
+      rightTrig = false;
+      Serial.println("Right gate reset");
+    }
+    if (middleHistory == 0b11111111111111111111 && middleTrig) {
+      Serial.println("Middle gate reset");
+      middleTrig = false;
+    }
+    // Update histories
+    leftHistory = leftHistory << 1;
+    leftHistory &= 0b11111111111111111111;
+    leftHistory |= leftLightInput;
+    middleHistory = middleHistory << 1;
+    middleHistory &= 0b11111111111111111111;;
+    middleHistory |= middleLightInput;
+    rightHistory = rightHistory << 1;
+    rightHistory &= 0b11111111111111111111;;
+    rightHistory |= rightLightInput;
 }
 
 void changeState() {
-  pprint("IN", inCount);
-  pprint("OUT", outCount);
-
-  if (inCount >= outCount * 2 && middleGateState == OUT) {
-    // Too many INs
-    middleGateState = IN;
-    digitalWrite(middleLEDPin, HIGH);
-    Serial.println("Changing state to IN");
-  } else if (outCount >= inCount * 2 && middleGateState == IN) {
-    // Too many OUTs
-    middleGateState = OUT;
-    digitalWrite(middleLEDPin, LOW);
-    Serial.println("Changing state to OUT");
-  } else {
-    // Else gate will stay the same
-    pprinte("Keeping state the same", middleGateState);
-  }
-
-  // Reseting counts
-  inCount = 1;
-  outCount = 1;
+    pprint("IN", inCount);
+    pprint("OUT", outCount);
+    if (inCount >= outCount * 2 && middleGateState == OUT) {
+      // Too many INs
+      middleGateState = IN;
+      digitalWrite(middleLEDPin, HIGH);
+      Serial.println("Changing state to IN");
+      sendEvent(2 , "IN" , inCount , outCount);
+    } else if (outCount >= inCount * 2 && middleGateState == IN) {
+      // Too many OUTs
+      middleGateState = OUT;
+      digitalWrite(middleLEDPin, LOW);
+      Serial.println("Changing state to OUT");
+      sendEvent(2 , "OUT" , inCount , outCount);
+    } else {
+      // Else gate will stay the same
+      pprinte("Keeping state the same", middleGateState);
+      sendEvent(2 , (String)middleGateState, inCount , outCount);
+    }
+    // Reseting counts
+    inCount = 1;
+    outCount = 1;
 }
 
 void pprint(String label, int val) {
-  String toPrint = label + ": ";
-  toPrint += val;
-  Serial.println(toPrint);
+    String toPrint = label + ": ";
+    toPrint += val;
+    Serial.println(toPrint);
 }
 
 void pprinte(String label, enum state val) {
-  String toPrint = label + ": ";
-  toPrint += val;
-  Serial.println(toPrint);
+    String toPrint = label + ": ";
+    toPrint += val;
+    Serial.println(toPrint);
 }
 
 void wifiSetup() {
@@ -164,3 +156,57 @@ void wifiSetup() {
   Serial.println(WiFi.localIP());
 }
 
+void sendEvent(int gateID , String gateStatus , int riderCountIN , int riderCountOUT) {
+    String json = buildJSON(gateID , gateStatus ,riderCountIN , riderCountOUT);
+    Serial.println("This is the json file: " + json + "\n");
+    Serial.println("Requesting POST: ");
+     // Send request to the server:
+     if (client.connect(host, 80)) {
+       Serial.println("Connected to host!");
+       client.println("POST /endpoint HTTP/1.1");
+       client.println("Host: http://iot-gate-proxy.azurewebsites.net");
+       client.println("Accept: */*");
+       client.println("Content-Type: application/x-www-form-urlencoded");
+       client.print("Content-Length: ");
+       client.println(json.length());
+       client.println();
+       client.println(json);
+       client.println("Connection: close");
+     }
+
+     unsigned long timeout = millis();
+  while (client.available() == 0) {
+    yield();
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+     
+      // Read all the lines of the reply from server and print them to Serial
+      Serial.println("Response:");
+      while(client.available()){
+        yield();
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+      }
+    
+     Serial.println();
+     Serial.println("closing connection");
+     
+ }
+
+String buildJSON(int gateID , String gateStatus , int riderCountIN , int riderCountOUT) {
+  // {"endpoint": "smartGatesEvent__e","payload": {"gateID__c": 1,"gateStatus__c": "IN","riderCountIN__c": 1,"riderCountOUT__c": 0}}
+  String json = "{\"endpoint\":\"smartGatesEvent__e\",\"payload\": {\"gateID__c\":";
+  json+=gateID;
+  json+=",\"gateStatus__c\":";
+  json+=gateStatus;
+  json+=",\"riderCountIN__c\":";
+  json+=riderCountIN;
+  json+=",\"riderCountOUT__c\":";
+  json+=riderCountOUT;
+  json+="}}";
+  return json;
+}
